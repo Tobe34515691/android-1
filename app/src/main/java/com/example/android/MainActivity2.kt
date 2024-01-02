0b4cf38bf928aeaa15be9f1899a1ec942b2dd3c1
user <123@>
2024-01-02 18:02:27 +0800

    新3

--- app/src/main/java/com/example/android/MainActivity2.kt
+++ app/src/main/java/com/example/android/MainActivity2.kt
@@ -2,10 +2,29 @@ package com.example.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.databinding.ActivityMain2Binding
+import com.example.android.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding1: ActivityMain2Binding
+    private var z="123"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding1 =ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding1.root)
+        z= intent.getStringExtra("z").toString()
+
+        binding1.a17.alpha=0f
+        binding1.a18.alpha=0f
+        binding1.textView2.alpha=0f
+
+        if(z=="7"){
+            binding1.a18.alpha=1f
+        }else if(z=="8"){
+            binding1.a17.alpha=1f
+        }else{
+            binding1.textView2.text="目前不再大樓"
+            binding1.textView2.alpha=1f
+        }
     }
 }
