a2faf5ef91d2f7b61e71d066c1cf7c7c28ea50d6
11010141 <11010141@gm.chihlee.edu.tw>
2024-01-03 10:11:49 +0800

    圖片程式完成

--- app/src/main/java/com/example/android/MainActivity2.kt
+++ app/src/main/java/com/example/android/MainActivity2.kt
@@ -2,10 +2,35 @@ package com.example.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.databinding.ActivityMain2Binding
+import com.example.android.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding1: ActivityMain2Binding
+    private var z="123"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding1 =ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding1.root)
+        z= intent.getStringExtra("z").toString()
+
+        binding1.a14.alpha=0f
+        binding1.a15.alpha=0f
+        binding1.a16.alpha=0f
+
+
+         if(z=="4") {
+            binding1.a14.alpha = 1f
+        }else if(z=="5"){
+            binding1.a15.alpha=1f
+        }else if(z=="6"){
+            binding1.a16.alpha=1f
+
+        }
+        }
     }
-}
+
+
+
+
+
